122a3635852510e76945edb44af9d19cc0e6278e
minju8843 <74241186+minju8843@users.noreply.github.com>
2024-11-12 01:20:06 +0900

    Add files via upload

---
+++ Script/Main/Save_Collection.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Save_Collection : MonoBehaviour
+{
+    // < 여기부터 엔딩 4개 >
+
+    //엔딩1
+    public GameObject[] Ending1;
+    public int Ending1_Index;
+
+    //엔딩2
+    public GameObject[] Ending2;
+    public int Ending2_Index;
+
+    //엔딩3
+    public GameObject[] Ending3;
+    public int Ending3_Index;
+
+    //엔딩4
+    public GameObject[] Ending4;
+    public int Ending4_Index;
+
+
+    // < 여기부터 캐릭터 4개 >
+
+    //사장
+    public GameObject[] Boss;
+    public int Boss_Index;
+
+    //소리
+    public GameObject[] Sori;
+    public int Sori_Index;
+
+    //가짜 사장
+    public GameObject[] Fake_Boss;
+    public int Fake_Boss_Index;
+
+    //조율자
+    public GameObject[] Black_Face_Man;
+    public int Black_Face_Man_Index;
+
+    // < 여기부터 타임라인 얼마나 열렸나 >
+
+    //큰 네모 - 챕터 1 타임라인
+    public GameObject[] Time_Line_Open;
+    public int Time_Line_Open_Index;
+
+
+    //중간부터 시작하는 거
+    public GameObject[] Time_Run;
+    public int Time_Run_Index;
+
+    //타임라인 안에 있는 동그라미1
+    public GameObject[] Time_Circle_1;
+    public int Time_Circle_1_Index;
+
+    //타임라인 안에 있는 동그라미2
+    public GameObject[] Time_Circle_2;
+    public int Time_Circle_2_Index;
+
+
+
+    //몇 번째 줄인지 저장하는 건 이미 있음
+
+    //업적 몇 개 열렸는지
+    //업적 하나당 아예 닫힘, 힌트, 열림이 있음
+    public GameObject[] Achieve_1;
+    public int Achieve1_Index;
+
+    public GameObject[] Achieve_2;
+    public int Achieve2_Index;
+
+    public GameObject[] Achieve_3;
+    public int Achieve3_Index;
+
+    public GameObject[] Achieve_4;
+    public int Achieve4_Index;
+
+    public GameObject[] Achieve_5;
+    public int Achieve5_Index;
+
+    public GameObject[] Achieve_6;
+    public int Achieve6_Index;
+
+    public GameObject[] Achieve_7;
+    public int Achieve7_Index;
+
+    public GameObject[] Achieve_8;
+    public int Achieve8_Index;
+
+    public GameObject[] Achieve_9;
+    public int Achieve9_Index;
+
+    public GameObject[] Achieve_10;
+    public int Achieve10_Index;
+
+    public GameObject[] Achieve_11;
+    public int Achieve11_Index;
+
+    public GameObject[] Achieve_12;
+    public int Achieve12_Index;
+
+    //앨범 몇 개 열렸는지
+    public GameObject[] Album_1;
+    public int Album1_Index;
+
+    public GameObject[] Album_2;
+    public int Album2_Index;
+
+    public GameObject[] Album_3;
+    public int Album3_Index;
+
+    public GameObject[] Album_4;
+    public int Album4_Index;
+
+    public GameObject[] Album_5;
+    public int Album5_Index;
+
+    public GameObject[] Album_6;
+    public int Album6_Index;
+
+    public GameObject[] Album_7;
+    public int Album7_Index;
+
+    public GameObject[] Album_8;
+    public int Album8_Index;
+
+    public GameObject[] Album_9;
+    public int Album9_Index;
+
+    public GameObject[] Album_10;
+    public int Album10_Index;
+
+    public GameObject[] Album_11;
+    public int Album11_Index;
+
+    public GameObject[] Album_12;
+    public int Album12_Index;
+
+
+}
